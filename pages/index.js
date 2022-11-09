3558ea78d05d6d424f255ce42399874c050f3a9e
Matheus <matheusvargas042@gmail.com>
2022-11-09 15:58:07 -0300

    aula 3

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import config from '../config.json';
 import styled from 'styled-components';
-import { CSSReset } from '../src/components/CSSReset';
 import Menu from '../src/components/Menu';
 import { StyledTimeline } from '../src/components/Timeline';
 
@@ -11,7 +10,6 @@ function HomePage() {
 
     return (
         <>
-            <CSSReset />
             <div style={{
                 display: "flex",
                 flexDirection: "column",
@@ -29,6 +27,8 @@ function HomePage() {
 export default HomePage;
 
 const StyledHeader = styled.div`
+    background-color: ${({ theme }) => theme.backgroundLevel1};
+    
     img {
         width: 80px;
         height: 80px;
